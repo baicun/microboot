e4881aef268a31899c5867d53fdb9cf66897ebdc
baicun <javaman_baicun@126.com>
2018-09-26 00:02:03 +0800

    增加测试

---
+++ base/src/main/java/cn/mldn/controller/BaseController2.java
@@ -0,0 +1,16 @@
+package cn.mldn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by super on 2018/9/19.
+ */
+@RestController
+public class BaseController2 {
+
+    @RequestMapping(value = "/hello")
+    public String base(){
+        return "peace World";
+    }
+}
